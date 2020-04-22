1708be21ba039bc6579b584803f23c825d32f352
George Roger <georger@graphcore.ai>
2020-04-22 16:43:58 +0100

    Use cmake `find_library` function so that it finds the correct library on osx.
    Summary: Previous code was looking for libtorch.so, but on osx we need libtorch.dylib.
    
    Reviewers: #pytorch, jamesn
    
    Reviewed By: #pytorch, jamesn
    
    Maniphest Tasks: T19543
    
    Differential Revision: https://phabricator.sourcevertex.net/D24691

--- cmake/Modules/FindTorch.cmake
+++ cmake/Modules/FindTorch.cmake
@@ -4,14 +4,17 @@ something like -DTorch_DIR=/path/to/directory/containing/TorchConfig.cmake/")
 execute_process(COMMAND python -c "import torch; from pathlib import Path; print(Path(torch.__file__).parent, end='')"
                 OUTPUT_VARIABLE TorchInit_PATH)
 
-FIND_PATH(TorchLibsPath
-  HINTS ${TorchInit_PATH}/lib
-  NAMES libtorch.so)
-IF(NOT TorchLibsPath)
-  MESSAGE(FATAL_ERROR "Could not find libtorch.so. ${WhatToDoString}")
-ENDIF()
-set(TORCH_LIBRARIES "${TorchLibsPath}/libtorch.so")
-list(APPEND TORCH_LIBRARIES "${TorchLibsPath}/libtorch_python.so")
+find_library(LibTorch torch ${TorchInit_PATH}/lib)
+if (NOT LibTorch)
+  message(FATAL_ERROR "Could not find shared library for torch.")
+endif()
+
+find_library(LibTorchPython torch_python ${TorchInit_PATH}/lib)
+if (NOT LibTorchPython)
+  message(FATAL_ERROR "Could not find shared library for torch_python.")
+endif()
+
+set(TORCH_LIBRARIES "${LibTorch};${LibTorchPython}")
 
 # Include dirs
 if (EXISTS "${TorchInit_PATH}/include")
