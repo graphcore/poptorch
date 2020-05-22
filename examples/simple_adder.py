f9b74b5d0fb8ed97dd106eb7b0d9467056ae8392
David Norman <davidn@graphcore.ai>
2020-05-22 11:07:36 +0100

    Add a single simple example
    Summary:
    Adding a simple example, something that can be used to verify
    correct installation, and be included in the docs.
    
    Test Plan: All tests passing
    
    Reviewers: #pytorch, stephenm
    
    Reviewed By: #pytorch, stephenm
    
    Subscribers: stephenm
    
    Differential Revision: https://phabricator.sourcevertex.net/D26300

---
+++ examples/simple_adder.py
@@ -0,0 +1,22 @@
+import torch
+import torch.nn as nn
+import poptorch
+
+# This simple example demoonstrates compiling a model to add
+# two tensors together using the IPU.
+
+class SimpleAdder(nn.Module):
+    def __init__(self):
+        super(SimpleAdder, self).__init__()
+
+    def forward(self, x, y):
+        return x + y
+
+model = SimpleAdder()
+inference_model = poptorch.inferenceModel(model)
+
+t1 = torch.tensor([1.])
+t2 = torch.tensor([2.])
+
+assert inference_model((t1, t2)) == 3.0
+print("Success")
