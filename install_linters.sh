131c7d41e035b2735ac645c933c6311795f6eed9
Anthony Barbier <anthonyb@graphcore.ai>
2020-07-09 10:03:19 +0100

    clang-format: Use pip for linux and download from LLVM website for OSX
    Reviewers: #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, #pytorch, hatemh, stephenm
    
    Reviewed By: #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, #pytorch, hatemh, stephenm
    
    Subscribers: hatemh
    
    Maniphest Tasks: T22537
    
    Differential Revision: https://phabricator.sourcevertex.net/D28917

--- install_linters.sh
+++ install_linters.sh
@@ -10,6 +10,29 @@ then
   echo "Linters not installed: run ${DIR}/install_linters.sh to install the linters then try again"
   exit 1
 fi
+
+install_clang_format_apple() {
+  CLANG_PLATFORM=darwin-apple
+  BINARY=clang-format
+  CLANG_VERSION=9.0.0
+  FOLDER_NAME=clang+llvm-${CLANG_VERSION}-x86_64-${CLANG_PLATFORM}
+  ARCHIVE_NAME=${FOLDER_NAME}.tar.xz
+  URL=https://releases.llvm.org/${CLANG_VERSION}/${ARCHIVE_NAME}
+  VERSIONED_BINARY="${DIR}/.linters/${BINARY}-${CLANG_VERSION}"
+
+  if [ ! -f "${VERSIONED_BINARY}" ]; then
+    TMP_DIR=`mktemp -d`
+    pushd ${TMP_DIR}
+    curl -O $URL
+    tar xf ${ARCHIVE_NAME} ${FOLDER_NAME}/bin/clang-format
+    mv ${FOLDER_NAME}/bin/clang-format ${VERSIONED_BINARY}
+    chmod +x ${VERSIONED_BINARY}
+    popd
+    rm -rf ${TMP_DIR}
+  fi
+  ln -fs ${VERSIONED_BINARY} ${DIR}/.linters/clang-format
+}
+
 mkdir -p .linters
 
 VE=${DIR}/.linters/venv
@@ -22,9 +45,17 @@ fi
 source ${VE}/bin/activate
 pip install yapf==0.27.0
 pip install cpplint==1.4.4
-pip install clang-format==9.0.0
+
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+  pip install clang-format==9.0.0
+  ln -fs ${VE}/bin/clang-format ${DIR}/.linters/clang-format
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  install_clang_format_apple
+else
+  echo "ERROR: '${OSTYPE}' platform not supported"
+  exit -1
+fi
 
 ln -fs ${VE}/bin/yapf ${DIR}/.linters/yapf
 ln -fs ${VE}/bin/cpplint ${DIR}/.linters/cpplint
-ln -fs ${VE}/bin/clang-format ${DIR}/.linters/clang-format
 
