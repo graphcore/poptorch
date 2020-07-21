7c71db40125f78d58627f3af2e8e2b3c38cd9d52
Hatem Helal <hatemh@graphcore.ai>
2020-07-21 21:12:37 +0100

    Add support for hardtanh activation function
    Summary: This change maps both `aten::hardtanh` and `aten::hardtanh_` to the `clip` op in popart
    
    Reviewers: #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, #pytorch, anthonyb
    
    Reviewed By: #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, #pytorch, anthonyb
    
    Subscribers: anthonyb
    
    Maniphest Tasks: T24178
    
    Differential Revision: https://phabricator.sourcevertex.net/D29476

--- tests/activations.py
+++ tests/activations.py
@@ -19,7 +19,8 @@ import pytest
 activation_functions = [
     torch.nn.ReLU, torch.nn.Tanh, torch.nn.Sigmoid, torch.nn.PReLU,
     torch.nn.SELU, torch.nn.ELU, torch.nn.GELU, torch.nn.Softmax,
-    torch.nn.LogSoftmax, torch.nn.Softsign, torch.nn.LeakyReLU
+    torch.nn.LogSoftmax, torch.nn.Softsign, torch.nn.LeakyReLU,
+    torch.nn.Hardtanh
 ]
 
 
