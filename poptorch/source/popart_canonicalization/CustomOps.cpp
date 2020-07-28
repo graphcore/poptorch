4416e77fe4c0fcb71a34c3e0f680c1bde176a3ea
Stephen McGroarty <stephenm@graphcore.ai>
2020-07-28 17:52:36 +0100

    Missing file from custom op patch
    Summary: fixes T19199
    
    Reviewers: #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, #pytorch, anthonyb, hatemh
    
    Reviewed By: #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, #pytorch, anthonyb, hatemh
    
    Subscribers: hatemh
    
    Maniphest Tasks: T19199
    
    Differential Revision: https://phabricator.sourcevertex.net/D29770

---
+++ poptorch/source/popart_canonicalization/CustomOps.cpp
@@ -0,0 +1,42 @@
+// Copyright (c) 2020 Graphcore Ltd. All rights reserved.
+#include "../PoptorchSymbols.hpp"
+#include "PopartCanonicalizationUtils.hpp"
+#include "poptorch/OpBuilder.hpp"
+#include "poptorch_logging/Error.hpp"
+
+namespace poptorch {
+namespace {
+
+torch::jit::Node *customOpHandler(torch::jit::Graph *graph,
+                                  torch::jit::Node *node) {
+  std::vector<torch::jit::Value *> inputs =
+      handleTensorList(node->input(0)->node());
+  std::string name = *handleConstant<std::string>(node->input(1)->node());
+  std::string domain = *handleConstant<std::string>(node->input(2)->node());
+
+  // Get the domain version.
+  std::int64_t domain_version =
+      *handleConstant<std::int64_t>(node->input(3)->node());
+
+  // Get the number of outputs.
+  std::int64_t num_outputs =
+      *handleConstant<std::int64_t>(node->input(4)->node());
+
+  // Add the custom op with a variadic number of outputs.
+  torch::jit::Node *custom_op = createCustomOperation(
+      graph, inputs, name, domain, domain_version, num_outputs);
+
+  // It is replacing an operation which returned a list so add a list
+  // construct to keep the IR legal.
+  return createAndInsertNode(graph, at::prim::ListConstruct,
+                             custom_op->outputs());
+}
+
+} // namespace
+
+// clang-format off
+static bool handlers = registerHandlers(
+    symbols::poptorch::custom_operation, customOpHandler);
+// clang-format on
+
+} // namespace poptorch
