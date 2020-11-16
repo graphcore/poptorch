1d56c87ddf28400bd71d43256dd7f11e6cde9b3e
Ahmad Sarvmeily <ahmads@graphcore.ai>
2020-11-16 16:41:58 +0000

    Support torch.meshgrid
    Reviewers: #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, #pytorch, anthonyb
    
    Reviewed By: #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, #pytorch, anthonyb
    
    Maniphest Tasks: T29875
    
    Differential Revision: https://phabricator.sourcevertex.net/D35898

--- tests/other_ops_test.py
+++ tests/other_ops_test.py
@@ -41,3 +41,28 @@ def test_einsum(params, implicit_rhs):
 
     assert native_out.size() == poptorch_out.size()
     torch.testing.assert_allclose(native_out, poptorch_out)
+
+
+@pytest.mark.parametrize("arr_lengths",
+                         ([3], [3, 3], [2, 4], [3, 2, 4], [5, 2, 3, 4]))
+def test_meshgrid(arr_lengths):
+    torch.manual_seed(42)
+
+    class Model(torch.nn.Module):
+        def forward(self):
+            return torch.meshgrid(xs)
+
+    model = Model()
+    poptorch_model = poptorch.inferenceModel(model)
+
+    xs = [torch.randn(arr_length) for arr_length in arr_lengths]
+
+    # Run on CPU
+    native_out = model()
+
+    # Run on IPU
+    poptorch_out = poptorch_model()
+
+    for native, pop in zip(native_out, poptorch_out):
+        assert native.size() == pop.size()
+        torch.testing.assert_allclose(native, pop)
