bcf6b0a77f7b9416a71bd8bf9f77e654fa8a5afa
Ahmad Sarvmeily <ahmads@graphcore.ai>
2020-11-16 16:41:58 +0000

    Support torch.meshgrid
    Reviewers: #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, #pytorch, anthonyb
    
    Reviewed By: #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, #pytorch, anthonyb
    
    Maniphest Tasks: T29875
    
    Differential Revision: https://phabricator.sourcevertex.net/D35898

--- poptorch/source/popart_canonicalization/OtherOps.cpp
+++ poptorch/source/popart_canonicalization/OtherOps.cpp
@@ -20,11 +20,43 @@ torch::jit::Node *einsumHandler(torch::jit::Graph *graph,
   EinsumOp einsum(eq, tensors);
   return einsum.create(graph);
 }
+
+torch::jit::Node *meshgridHandler(torch::jit::Graph *graph,
+                                  torch::jit::Node *node) {
+  // aten::meshgrid(Tensor[] tensors) -> Tensor[]
+
+  std::vector<torch::jit::Value *> tensors =
+      handleTensorList(node->input(0)->node());
+
+  std::vector<std::int64_t> expand_shape;
+  expand_shape.reserve(tensors.size());
+  for (torch::jit::Value *tensor : tensors) {
+    // Each tensor is 1D so the shape is just the first dim
+    expand_shape.push_back(shapeFromTensor(tensor)[0]);
+  }
+
+  std::vector<torch::jit::Value *> grids;
+  for (std::size_t i = 0; i < tensors.size(); i++) {
+    std::vector<std::int64_t> shape(tensors.size(), 1);
+    shape[i] = -1;
+    // Reshape 1D tensor to rank N, N = number of tensors, such that
+    // all but the ith dimension is a singleton
+    torch::jit::Node *reshaped = createReshape(graph, tensors[i], shape);
+    // Expand over the dimensions of all other tensors
+    torch::jit::Node *expanded =
+        createExpand(graph, {reshaped->output(),
+                             intVectorToIrConstant(graph, expand_shape)});
+    grids.push_back(expanded->output());
+  }
+
+  return createAndInsertNode(graph, at::prim::ListConstruct, grids);
+}
 } // namespace
 
 // clang-format off
 static bool handlers = registerHandlers(
-    c10::aten::einsum, einsumHandler);
+    c10::aten::einsum, einsumHandler,
+    c10::aten::meshgrid, meshgridHandler);
 // clang-format on
 
 } // namespace poptorch
