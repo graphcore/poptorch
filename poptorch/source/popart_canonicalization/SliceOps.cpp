d10156191e5874c2140cc1b9a7e4516b8a14b9d7
Ahmad Sarvmeily <ahmads@graphcore.ai>
2021-01-25 14:24:26 +0000

    Support aten::index
    Summary: Indexing using boolean tensor masks is unsupported until NonZero is implemented in PopART. Indexing using byte tensor masks is also unsupported as the type is not supported in poplar.
    
    Reviewers: #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, #pytorch, nicholasw
    
    Reviewed By: #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, #pytorch, nicholasw
    
    Subscribers: nicholasw
    
    Maniphest Tasks: T31742
    
    Differential Revision: https://phabricator.sourcevertex.net/D39270

--- poptorch/source/popart_canonicalization/SliceOps.cpp
+++ poptorch/source/popart_canonicalization/SliceOps.cpp
@@ -48,10 +48,112 @@ torch::jit::Node *sliceHandler(torch::jit::Graph *graph,
                              wrapInConstant1D(graph, dim)});
 }
 
+torch::jit::Node *indexHandler(torch::jit::Graph *graph,
+                               torch::jit::Node *node) {
+  // aten::index(Tensor self, Tensor?[] indices)
+  torch::jit::Value *x = node->input(0);
+  std::vector<torch::jit::Value *> indices =
+      handleTensorList(node->input(1)->node());
+  auto shape = shapeFromTensor(x);
+  if (indices.size() > shape.size()) {
+    std::stringstream ss;
+    ss << "too many indices for tensor of dimension " << shape.size()
+       << " (got " << indices.size() << ")";
+    ERROR(ss.str());
+  }
+  std::vector<std::int64_t> final_index_shape = shapeFromTensor(node->output());
+
+  std::size_t pad = 0;
+  std::size_t index_size = 0;
+  bool indexed = false;
+  bool pad_front = true;
+  // Calculate the final index shape. This is the subset of the
+  // tensor that the gather operation will be performed on
+  for (torch::jit::Value *index : indices) {
+    if (isNone(index)) {
+      if (indexed) {
+        pad_front = false;
+      }
+      pad++;
+    } else {
+      if (getNodeScalarType(index) == c10::ScalarType::Bool) {
+        ERROR("Indexing using boolean/byte tensor masks is unsupported.");
+      }
+
+      auto s = shapeFromTensor(index);
+      if (s.size() > index_size) {
+        index_size = s.size();
+      }
+      indexed = true;
+    }
+  }
+  final_index_shape.resize(index_size + pad);
+
+  std::size_t nones_indexed = 0;
+  // Reshape each tensor into shape broadcastable with final output shape
+  for (std::size_t i = 0; i < indices.size(); i++) {
+    if (isNone(indices[i])) {
+      // Optional tensors: 'None' means indexing over entire dimension
+      // Replace each None tensor with its explicit index representation
+      std::vector<std::int64_t> idx(shape[i]);
+      std::iota(idx.begin(), idx.end(), 0);
+
+      std::vector<std::int64_t> new_shape(final_index_shape.size(), 1);
+      auto final_shape_index =
+          pad_front ? nones_indexed : index_size + nones_indexed;
+      new_shape[final_shape_index] = shape[i];
+      nones_indexed++;
+
+      indices[i] =
+          createReshape(graph, intVectorToIrConstant(graph, idx), new_shape)
+              ->output();
+    } else {
+      const auto index_shape = shapeFromTensor(indices[i]);
+      std::vector<std::int64_t> new_shape;
+
+      auto ones_generator = []() { return 1; };
+      if (pad_front) {
+        std::generate_n(std::back_inserter(new_shape), pad, ones_generator);
+      }
+      std::copy(index_shape.begin(), index_shape.end(),
+                std::back_inserter(new_shape));
+      if (!pad_front) {
+        std::generate_n(std::back_inserter(new_shape), pad, ones_generator);
+      }
+      indices[i] = createReshape(graph, indices[i], new_shape)->output();
+    }
+  }
+
+  auto flat_indices = createConstantInt(graph, {0}, {});
+  std::int64_t stride = 1;
+  // Calculate indices within partially flattened shape
+  // Tensors are automatically broadcast to the correct shape during calculation
+  for (std::size_t i = 0; i < indices.size(); i++) {
+    auto offset = indices[indices.size() - i - 1];
+    if (i != 0) {
+      offset =
+          createMul(graph, {offset, wrapInConstant1D(graph, stride)})->output();
+    }
+    flat_indices = createAdd(graph, {flat_indices->output(), offset});
+    stride *= shape[indices.size() - i - 1];
+  }
+
+  std::vector<std::int64_t> gather_shape = {-1};
+  std::copy_n(shape.begin() + indices.size(), shape.size() - indices.size(),
+              std::back_inserter(gather_shape));
+  // Flatten the tensor being indexed into [-1, u1, u2, ..., uN] where
+  // each u is a dimension not being indexed into
+  auto flatten = createReshape(graph, x, gather_shape);
+  // Gather in first dimension using calculated indices into partially flattened
+  // tensor
+  return createGather(graph, {flatten->output(), flat_indices->output()}, 0);
+}
+
 } // namespace
 
 __attribute__((constructor(HANDLER_INIT_PRIORITY))) static void registration() {
   registerHandler(c10::aten::slice, sliceHandler);
+  registerHandler(c10::aten::index, indexHandler);
 }
 
 } // namespace poptorch
--- tests/slice_test.py
+++ tests/slice_test.py
@@ -2,6 +2,8 @@
 # Copyright (c) 2020 Graphcore Ltd. All rights reserved.
 
 import torch
+import pytest
+
 import poptorch
 
 
@@ -61,3 +63,97 @@ def test_slice_with_branch():
 
     slice_test_harness(torch.tensor([1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0]),
                        torch.tensor([-3.0]), start_fn, end_fn)
+
+
+def index_test_harness(model, idxs):
+    t = torch.arange(120).reshape(2, 3, 4, 5)
+    idxs_tensors = []
+    for i in idxs:
+        idxs_tensors.append(torch.tensor(i))
+    idxs_tuple = tuple(idxs_tensors)
+    poptorch_model = poptorch.inferenceModel(model)
+
+    # Run on CPU
+    native_out = model(t, *idxs_tuple)
+    # Run on IPU
+    poptorch_out = poptorch_model(t, *idxs_tuple)
+
+    assert native_out.size() == poptorch_out.size()
+    torch.testing.assert_allclose(poptorch_out, native_out)
+
+
+@pytest.mark.parametrize("idxs",
+                         ([[0]], [[1]], [[0, 1]], [[1, 0]], [[[0, 1], [1, 0]]])
+                         )
+def test_index_1(idxs):
+    class Model(torch.nn.Module):
+        def forward(self, t, idx):
+            return t[idx]
+
+    index_test_harness(Model(), idxs)
+
+
+@pytest.mark.parametrize("idxs",
+                         ([[0]], [[1]], [[0, 1]], [[1, 0]], [[[0, 1], [1, 0]]])
+                         )
+def test_index_2(idxs):
+    class Model(torch.nn.Module):
+        def forward(self, t, idx):
+            return t[idx, idx]
+
+    index_test_harness(Model(), idxs)
+
+
+@pytest.mark.parametrize("idxs",
+                         ([[0]], [[1]], [[0, 1]], [[1, 0]], [[[0, 1], [1, 0]]])
+                         )
+def test_index_3(idxs):
+    class Model(torch.nn.Module):
+        def forward(self, t, idx):
+            return t[:, idx]
+
+    index_test_harness(Model(), idxs)
+
+
+@pytest.mark.parametrize("idxs",
+                         ([[0]], [[1]], [[0, 1]], [[1, 0]], [[[0, 1], [1, 0]]])
+                         )
+def test_index_4(idxs):
+    class Model(torch.nn.Module):
+        def forward(self, t, idx):
+            return t[idx, :, idx]
+
+    index_test_harness(Model(), idxs)
+
+
+@pytest.mark.parametrize("idxs",
+                         ([[0]], [[1]], [[0, 1]], [[1, 0]], [[[0, 1], [1, 0]]])
+                         )
+def test_index_5(idxs):
+    class Model(torch.nn.Module):
+        def forward(self, t, idx):
+            return t[:, :, idx]
+
+    index_test_harness(Model(), idxs)
+
+
+@pytest.mark.parametrize("idxs",
+                         ([[0]], [[1]], [[0, 1]], [[1, 0]], [[[0, 1], [1, 0]]])
+                         )
+def test_index_6(idxs):
+    class Model(torch.nn.Module):
+        def forward(self, t, idx):
+            return t[:, idx, idx]
+
+    index_test_harness(Model(), idxs)
+
+
+@pytest.mark.parametrize("idxs",
+                         ([[0]], [[1]], [[0, 1]], [[1, 0]], [[[0, 1], [1, 0]]])
+                         )
+def test_index_7(idxs):
+    class Model(torch.nn.Module):
+        def forward(self, t, idx):
+            return t[:, idx, :, idx]
+
+    index_test_harness(Model(), idxs)
