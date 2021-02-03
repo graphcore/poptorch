98bfb2936bc7ede6e2c6714d6a6cf0c3d36afbf4
Ahmad Sarvmeily <ahmads@graphcore.ai>
2021-02-03 10:09:07 +0000

    Support aten::index_put_
    Reviewers: #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, #pytorch, anthonyb
    
    Reviewed By: #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, #pytorch, anthonyb
    
    Maniphest Tasks: T32818
    
    Differential Revision: https://phabricator.sourcevertex.net/D39646

--- poptorch/source/popart_canonicalization/IndexOps.cpp
+++ poptorch/source/popart_canonicalization/IndexOps.cpp
@@ -10,22 +10,32 @@
 namespace poptorch {
 namespace {
 
-torch::jit::Node *indexHandler(torch::jit::Graph *graph,
-                               torch::jit::Node *node) {
-  // aten::index(Tensor self, Tensor?[] indices)
-  torch::jit::Value *x = node->input(0);
-  std::vector<torch::jit::Value *> indices =
-      handleTensorList(node->input(1)->node());
-  auto shape = shapeFromTensor(x);
-  if (indices.size() > shape.size()) {
-    std::stringstream ss;
-    ss << "too many indices for tensor of dimension " << shape.size()
-       << " (got " << indices.size() << ")";
-    ERROR(ss.str());
+struct IndexInfo {
+  torch::jit::Value *x_partial_flat;
+  torch::jit::Value *indices_partial_flat;
+};
+
+std::vector<std::int64_t> padShape(const std::vector<std::int64_t> &shape,
+                                   std::size_t pad, bool pad_front) {
+  std::vector<std::int64_t> output_shape;
+  auto ones_generator = []() { return 1; };
+  if (pad_front) {
+    std::generate_n(std::back_inserter(output_shape), pad, ones_generator);
   }
+  std::copy(shape.begin(), shape.end(), std::back_inserter(output_shape));
+  if (!pad_front) {
+    std::generate_n(std::back_inserter(output_shape), pad, ones_generator);
+  }
+  return output_shape;
+}
+
+IndexInfo processIndex(torch::jit::Graph *graph, torch::jit::Value *x,
+                       std::vector<torch::jit::Value *> *p_indices) {
+  auto &indices = *p_indices;
+  auto shape = shapeFromTensor(x);
 
   std::size_t pad = 0;
-  std::size_t index_size = 0;
+  std::vector<std::int64_t> index_shape;
   bool indexed = false;
   bool pad_front = true;
   // Calculate the final index size with which the gather operation will be
@@ -42,12 +52,15 @@ torch::jit::Node *indexHandler(torch::jit::Graph *graph,
       }
 
       auto s = shapeFromTensor(index);
-      if (s.size() > index_size) {
-        index_size = s.size();
+      if (s.size() > index_shape.size()) {
+        index_shape = s;
       }
       indexed = true;
     }
   }
+  std::size_t index_size = index_shape.size();
+  std::vector<std::int64_t> flat_indices_shape =
+      padShape(index_shape, pad, pad_front);
 
   std::size_t nones_indexed = 0;
   // Reshape each tensor into shape broadcastable with final output shape
@@ -62,24 +75,17 @@ torch::jit::Node *indexHandler(torch::jit::Graph *graph,
       auto final_shape_index =
           pad_front ? nones_indexed : index_size + nones_indexed;
       new_shape[final_shape_index] = shape[i];
+      flat_indices_shape[final_shape_index] = shape[i];
       nones_indexed++;
 
       indices[i] =
           createReshape(graph, intVectorToIrConstant(graph, idx), new_shape)
               ->output();
     } else {
-      const auto index_shape = shapeFromTensor(indices[i]);
-      std::vector<std::int64_t> new_shape;
+      const auto original_shape = shapeFromTensor(indices[i]);
+      std::vector<std::int64_t> new_shape =
+          padShape(original_shape, pad, pad_front);
 
-      auto ones_generator = []() { return 1; };
-      if (pad_front) {
-        std::generate_n(std::back_inserter(new_shape), pad, ones_generator);
-      }
-      std::copy(index_shape.begin(), index_shape.end(),
-                std::back_inserter(new_shape));
-      if (!pad_front) {
-        std::generate_n(std::back_inserter(new_shape), pad, ones_generator);
-      }
       indices[i] = createReshape(graph, indices[i], new_shape)->output();
     }
   }
@@ -97,22 +103,90 @@ torch::jit::Node *indexHandler(torch::jit::Graph *graph,
     flat_indices = createAdd(graph, {flat_indices->output(), offset});
     stride *= shape[indices.size() - i - 1];
   }
+  // Retain the shape for downstream calculation
+  flat_indices =
+      createReshape(graph, flat_indices->output(), flat_indices_shape);
 
-  std::vector<std::int64_t> gather_shape = {-1};
+  std::vector<std::int64_t> flatten_shape = {-1};
   std::copy_n(shape.begin() + indices.size(), shape.size() - indices.size(),
-              std::back_inserter(gather_shape));
+              std::back_inserter(flatten_shape));
   // Flatten the tensor being indexed into [-1, u1, u2, ..., uN] where
   // each u is a dimension not being indexed into
-  auto flatten = createReshape(graph, x, gather_shape);
+  auto flatten = createReshape(graph, x, flatten_shape);
+
+  return {flatten->output(), flat_indices->output()};
+}
+
+torch::jit::Node *indexHandler(torch::jit::Graph *graph,
+                               torch::jit::Node *node) {
+  // aten::index(Tensor self, Tensor?[] indices)
+  torch::jit::Value *x = node->input(0);
+  std::vector<torch::jit::Value *> indices =
+      handleTensorList(node->input(1)->node());
+
+  IndexInfo info = processIndex(graph, x, &indices);
   // Gather in first dimension using calculated indices into partially flattened
   // tensor
-  return createGather(graph, {flatten->output(), flat_indices->output()}, 0);
+  return createGather(graph, {info.x_partial_flat, info.indices_partial_flat},
+                      0);
+}
+
+torch::jit::Node *indexPutHandler(torch::jit::Graph *graph,
+                                  torch::jit::Node *node) {
+  // aten::index_put_(Tensor self, Tensor?[] indices, Tensor value, bool
+  //                  accumulate)
+  torch::jit::Value *x = node->input(0);
+  std::vector<torch::jit::Value *> indices =
+      handleTensorList(node->input(1)->node());
+  torch::jit::Value *v = node->input(2);
+
+  auto fn_gen_none = [graph]() {
+    torch::jit::Value *none = graph->create(c10::prim::Constant)->output();
+    none->setType(c10::NoneType::get());
+    return none;
+  };
+  auto shape = shapeFromTensor(x);
+  // Scatter cannot assign entire dimensions, only individual elements, so we
+  // must pad the end of indices with NoneTypes so that the entire input is
+  // flattened during indexing
+  std::generate_n(std::back_inserter(indices), shape.size() - indices.size(),
+                  fn_gen_none);
+
+  IndexInfo info = processIndex(graph, x, &indices);
+
+  auto v_shape = shapeFromTensor(v);
+  auto indices_shape = shapeFromTensor(info.indices_partial_flat);
+  auto indices_size =
+      std::accumulate(indices_shape.begin(), indices_shape.end(), 1,
+                      std::multiplies<std::int64_t>{});
+
+  // Ensure value tensor can be broadcast with indexing result
+  if (v_shape.size() < indices_shape.size()) {
+    v = createReshape(graph, v, {1, -1})->output();
+    auto v_size = std::accumulate(v_shape.begin(), v_shape.end(), 1,
+                                  std::multiplies<std::int64_t>{});
+    // Repeat v to match indices shape
+    v = createExpand(graph, {v, intVectorToIrConstant(
+                                    graph, {indices_size / v_size, 1})})
+            ->output();
+  }
+  info.indices_partial_flat =
+      createReshape(graph, info.indices_partial_flat, {indices_size})->output();
+  v = createReshape(graph, v, {indices_size})->output();
+
+  // Scatter in first dimension using calculated indices into fully flattened
+  // tensor
+  auto scatter = createScatter(
+      graph, {info.x_partial_flat, info.indices_partial_flat, v}, 0);
+  // Restore original input shape
+  return createReshape(graph, scatter->output(), shape);
 }
 
 } // namespace
 
 __attribute__((constructor(HANDLER_INIT_PRIORITY))) static void registration() {
   registerHandler(c10::aten::index, indexHandler);
+  registerHandler(c10::aten::index_put_, indexPutHandler);
 }
 
 } // namespace poptorch
--- tests/index_ops_test.py
+++ tests/index_ops_test.py
@@ -8,66 +8,148 @@ import pytest
 import poptorch
 
 
+class IndexModel0(nn.Module):
+    def forward(self, t, idx, v=None):
+        if v is None:
+            return t[idx]
+        t += 0  # Ensure input is not modified in place
+        t[idx] = v
+        return t
+
+
 class IndexModel1(nn.Module):
-    def forward(self, t, idx):
-        return t[idx]
+    def forward(self, t, idx, v=None):
+        if v is None:
+            return t[idx, idx]
+        t += 0  # Ensure input is not modified in place
+        t[idx, idx] = v
+        return t
 
 
 class IndexModel2(nn.Module):
-    def forward(self, t, idx):
-        return t[idx, idx]
+    def forward(self, t, idx, v=None):
+        if v is None:
+            return t[:, idx]
+        t += 0  # Ensure input is not modified in place
+        t[:, idx] = v
+        return t
 
 
 class IndexModel3(nn.Module):
-    def forward(self, t, idx):
-        return t[:, idx]
+    def forward(self, t, idx, v=None):
+        if v is None:
+            return t[idx, :, idx]
+        t += 0  # Ensure input is not modified in place
+        t[idx, :, idx] = v
+        return t
 
 
 class IndexModel4(nn.Module):
-    def forward(self, t, idx):
-        return t[idx, :, idx]
+    def forward(self, t, idx, v=None):
+        if v is None:
+            return t[:, :, idx]
+        t += 0  # Ensure input is not modified in place
+        t[:, :, idx] = v
+        return t
 
 
 class IndexModel5(nn.Module):
-    def forward(self, t, idx):
-        return t[:, :, idx]
+    def forward(self, t, idx, v=None):
+        if v is None:
+            return t[:, idx, idx]
+        t += 0  # Ensure input is not modified in place
+        t[:, idx, idx] = v
+        return t
 
 
 class IndexModel6(nn.Module):
-    def forward(self, t, idx):
-        return t[:, idx, idx]
+    def forward(self, t, idx, v=None):
+        if v is None:
+            return t[idx, idx, idx, idx]
+        t += 0  # Ensure input is not modified in place
+        t[idx, idx, idx, idx] = v
+        return t
 
 
 class IndexModel7(nn.Module):
-    def forward(self, t, idx):
-        return t[:, idx, :, idx]
+    def forward(self, t, idx, v=None):
+        if v is None:
+            return t[:, :, :, idx]
+        t += 0  # Ensure input is not modified in place
+        t[:, :, :, idx] = v
+        return t
 
 
-@pytest.mark.parametrize("idxs",
-                         ([[0]], [[1]], [[0, 1]], [[1, 0]], [[[0, 1], [1, 0]]])
-                         )
-@pytest.mark.parametrize(
-    "model", {
-        IndexModel1(),
-        IndexModel2(),
-        IndexModel3(),
-        IndexModel4(),
-        IndexModel5(),
-        IndexModel6(),
-        IndexModel7()
-    })
-def test_index(model, idxs):
-    t = torch.arange(120).reshape(2, 3, 4, 5)
+class IndexModel8(nn.Module):
+    def forward(self, t, idx, v=None):
+        if v is None:
+            return t[:, idx, :, idx]
+        t += 0  # Ensure input is not modified in place
+        t[:, idx, :, idx] = v
+        return t
+
+
+def index_harness(model, idxs, pass_value, v=None):
+    t = torch.arange(120, dtype=torch.int32).reshape(2, 3, 4, 5)
     idxs_tensors = []
     for i in idxs:
         idxs_tensors.append(torch.tensor(i))
     idxs_tuple = tuple(idxs_tensors)
     poptorch_model = poptorch.inferenceModel(model)
 
-    # Run on CPU
-    native_out = model(t, *idxs_tuple)
-    # Run on IPU
-    poptorch_out = poptorch_model(t, *idxs_tuple)
+    if pass_value:
+        if v is None:
+            v = torch.empty_like(model(t, *idxs_tuple),
+                                 dtype=torch.int32).fill_(2 * 3 * 4 * 5)
+        native_out = model(t, *idxs_tuple, v)
+        poptorch_out = poptorch_model(t, *idxs_tuple, v)
+    else:
+        native_out = model(t, *idxs_tuple)
+        poptorch_out = poptorch_model(t, *idxs_tuple)
 
     assert native_out.size() == poptorch_out.size()
     torch.testing.assert_allclose(poptorch_out, native_out)
+
+
+index_models = (
+    IndexModel0(),
+    IndexModel1(),
+    IndexModel2(),
+    IndexModel3(),
+    IndexModel4(),
+    IndexModel5(),
+    IndexModel6(),
+    IndexModel7(),
+    IndexModel8(),
+)
+
+index_indices = ([[0]], [[1]], [[0, 1]], [[1, 0]], [[[0, 1], [1, 0]]])
+
+
+@pytest.mark.parametrize("idxs", index_indices)
+@pytest.mark.parametrize("model", index_models)
+def test_index(model, idxs):
+    index_harness(model, idxs, False)
+
+
+@pytest.mark.parametrize("idxs", index_indices)
+@pytest.mark.parametrize("model", index_models)
+def test_index_put(model, idxs):
+    index_harness(model, idxs, True)
+
+
+def test_index_put_assign_scalar():
+    class Model(nn.Module):
+        def forward(self, t, idx, v):
+            t += 0  # Ensure input is not modified in place
+            t[:, idx] = v.item()
+            return t
+
+    # For each element e in t[:, 0], e = 120
+    index_harness(Model(), [[0]], True, torch.tensor([2 * 3 * 4 * 5]))
+
+
+def test_index_put_assign_broadcastable():
+    v = torch.empty(5, dtype=torch.int32).fill_(2 * 3 * 4 * 5)
+    # For each row r in t[:, 0], r = [120, 120, 120, 120, 120]
+    index_harness(IndexModel2(), [[0]], True, v)
