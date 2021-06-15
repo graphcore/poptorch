96ddfd5ee5e57f399aa4882561c7315d2d2cc6d3
Ahmad Sarvmeily <ahmads@graphcore.ai>
2021-06-15 13:04:18 +0100

    Add training tests in index_ops_test.py
    Reviewers: #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, #pytorch, anthonyb
    
    Reviewed By: #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, #pytorch, anthonyb
    
    Maniphest Tasks: T38220
    
    Differential Revision: https://phabricator.sourcevertex.net/D46836

--- tests/index_ops_test.py
+++ tests/index_ops_test.py
@@ -2,175 +2,167 @@
 # Copyright (c) 2021 Graphcore Ltd. All rights reserved.
 
 import torch
-import torch.nn as nn
 import pytest
 
 import poptorch
 import helpers
 
 
-class IndexModel0(nn.Module):
-    def forward(self, t, idx, v=None):
-        if v is None:
-            return t[idx]
-        t += 0  # Ensure input is not modified in place
-        t[idx] = v
-        return t
+def index_op0(t, idx, v=None):
+    if v is None:
+        return t[idx]
+    t[idx] = v
+    return t
 
 
-class IndexModel1(nn.Module):
-    def forward(self, t, idx, v=None):
-        if v is None:
-            return t[idx, idx]
-        t += 0  # Ensure input is not modified in place
-        t[idx, idx] = v
-        return t
+def index_op1(t, idx, v=None):
+    if v is None:
+        return t[idx, idx]
+    t[idx, idx] = v
+    return t
 
 
-class IndexModel2(nn.Module):
-    def forward(self, t, idx, v=None):
-        if v is None:
-            return t[:, idx]
-        t += 0  # Ensure input is not modified in place
-        t[:, idx] = v
-        return t
+def index_op2(t, idx, v=None):
+    if v is None:
+        return t[:, idx]
+    t[:, idx] = v
+    return t
 
 
-class IndexModel3(nn.Module):
-    def forward(self, t, idx, v=None):
-        if v is None:
-            return t[idx, :, idx]
-        t += 0  # Ensure input is not modified in place
-        t[idx, :, idx] = v
-        return t
+def index_op3(t, idx, v=None):
+    if v is None:
+        return t[idx, :, idx]
+    t[idx, :, idx] = v
+    return t
 
 
-class IndexModel4(nn.Module):
-    def forward(self, t, idx, v=None):
-        if v is None:
-            return t[:, :, idx]
-        t += 0  # Ensure input is not modified in place
-        t[:, :, idx] = v
-        return t
+def index_op4(t, idx, v=None):
+    if v is None:
+        return t[:, :, idx]
+    t[:, :, idx] = v
+    return t
 
 
-class IndexModel5(nn.Module):
-    def forward(self, t, idx, v=None):
-        if v is None:
-            return t[:, idx, idx]
-        t += 0  # Ensure input is not modified in place
-        t[:, idx, idx] = v
-        return t
+def index_op5(t, idx, v=None):
+    if v is None:
+        return t[:, idx, idx]
+    t[:, idx, idx] = v
+    return t
 
 
-class IndexModel6(nn.Module):
-    def forward(self, t, idx, v=None):
-        if v is None:
-            return t[idx, idx, idx, idx]
-        t += 0  # Ensure input is not modified in place
-        t[idx, idx, idx, idx] = v
-        return t
+def index_op6(t, idx, v=None):
+    if v is None:
+        return t[idx, idx, idx, idx]
+    t[idx, idx, idx, idx] = v
+    return t
 
 
-class IndexModel7(nn.Module):
-    def forward(self, t, idx, v=None):
-        if v is None:
-            return t[:, :, :, idx]
-        t += 0  # Ensure input is not modified in place
-        t[:, :, :, idx] = v
-        return t
+def index_op7(t, idx, v=None):
+    if v is None:
+        return t[:, :, :, idx]
+    t[:, :, :, idx] = v
+    return t
 
 
-class IndexModel8(nn.Module):
-    def forward(self, t, idx, v=None):
-        if v is None:
-            return t[:, idx, :, idx]
-        t += 0  # Ensure input is not modified in place
-        t[:, idx, :, idx] = v
-        return t
+def index_op8(t, idx, v=None):
+    if v is None:
+        return t[:, idx, :, idx]
+    t[:, idx, :, idx] = v
+    return t
 
 
-def index_harness(model, idxs, pass_value, v=None):
-    t = torch.arange(120, dtype=torch.int32).reshape(2, 3, 4, 5)
-    idxs_tensors = []
-    for i in idxs:
-        idxs_tensors.append(torch.tensor(i))
-    idxs_tuple = tuple(idxs_tensors)
-    poptorch_model = poptorch.inferenceModel(model)
+def index_harness(op, idxs, pass_value, v=None):
+    torch.manual_seed(42)
+    t = torch.randn(2, 3, 4, 5)
+    idxs_tensors = [torch.tensor(i) for i in idxs]
+    model = helpers.ModelWithWeights(op, t.shape)
+    # The LR should be large enough to guarantee weights change
+    optim = torch.optim.AdamW(model.parameters(), lr=0.1)
+    poptorch_model = poptorch.trainingModel(model, optimizer=optim)
 
     if pass_value:
         if v is None:
-            v = torch.empty_like(model(t, *idxs_tuple),
-                                 dtype=torch.int32).fill_(2 * 3 * 4 * 5)
-        native_out = model(t, *idxs_tuple, v)
-        poptorch_out = poptorch_model(t, *idxs_tuple, v)
+            v = torch.zeros_like(op(t, *idxs_tensors))
+        # Clone the tensor so that the original is unchanged by the in-place op
+        native_out, _ = model((t.clone(), *idxs_tensors, v))
+        poptorch_out, _ = poptorch_model((t, *idxs_tensors, v))
     else:
-        native_out = model(t, *idxs_tuple)
-        poptorch_out = poptorch_model(t, *idxs_tuple)
+        native_out, _ = model((t, *idxs_tensors))
+        poptorch_out, _ = poptorch_model((t, *idxs_tensors))
 
-    assert native_out.size() == poptorch_out.size()
+    # Inference test - check outputs
     helpers.assert_allclose(actual=poptorch_out, expected=native_out)
 
+    # Training test - check weights changed
+    poptorch_model.assert_weights_changed()
 
-index_models = (
-    IndexModel0(),
-    IndexModel1(),
-    IndexModel2(),
-    IndexModel3(),
-    IndexModel4(),
-    IndexModel5(),
-    IndexModel6(),
-    IndexModel7(),
-    IndexModel8(),
-)
+
+index_ops = [
+    index_op0,
+    index_op1,
+    index_op2,
+    index_op3,
+    index_op4,
+    index_op5,
+    index_op6,
+    index_op7,
+    index_op8,
+]
+# TODO(T40635): Remove this list and use index_ops in test_index_put when
+# the index_put bug is fixed
+index_put_ops = [
+    index_op0,
+    index_op1,
+    index_op6,
+]
 
 index_indices = ([[0]], [[1]], [[0, 1]], [[1, 0]], [[[0, 1], [1, 0]]])
 
 
 @pytest.mark.parametrize("idxs", index_indices)
-@pytest.mark.parametrize("model", index_models)
-def test_index(model, idxs):
-    index_harness(model, idxs, False)
+@pytest.mark.parametrize("op", index_ops)
+def test_index(op, idxs):
+    index_harness(op, idxs, False)
 
 
 @pytest.mark.parametrize("idxs", index_indices)
-@pytest.mark.parametrize("model", index_models)
-def test_index_put(model, idxs):
-    index_harness(model, idxs, True)
+@pytest.mark.parametrize("op", index_put_ops)
+def test_index_put(op, idxs):
+    index_harness(op, idxs, True)
 
 
-def test_index_put_assign_scalar():
-    class Model(nn.Module):
-        def forward(self, t, idx, v):
-            t += 0  # Ensure input is not modified in place
-            t[:, idx] = v.item()
-            return t
+def test_index_put_scalar():
+    def op(t, idx, v):
+        t[idx, idx] = v.item()
+        return t
 
-    # For each element e in t[:, 0], e = 120
-    index_harness(Model(), [[0]], True, torch.tensor([2 * 3 * 4 * 5]))
+    # For each element e in t[0, 0], e = 0
+    index_harness(op, [[0]], True, torch.tensor([0]))
 
 
-def test_index_put_assign_broadcastable():
-    v = torch.empty(5, dtype=torch.int32).fill_(2 * 3 * 4 * 5)
-    # For each row r in t[:, 0], r = [120, 120, 120, 120, 120]
-    index_harness(IndexModel2(), [[0]], True, v)
+def test_index_put_broadcastable():
+    v = torch.zeros(5)
+    # For each row r in t[0, 0], r = [0, 0, 0, 0, 0]
+    index_harness(index_op1, [[0]], True, v)
 
 
 @pytest.mark.parametrize("dim", range(-3, 3))
 def test_index_select(dim):
-    class Model(nn.Module):
-        def forward(self, src, index):
-            return src.index_select(dim, index)
+    op = lambda src, index: src.index_select(dim, index)
 
     torch.manual_seed(0)
-    x = torch.randn(4, 16, 64)
+    x = torch.randn(2, 4, 8)
     sz = x.shape[dim]
     indices = torch.randint(sz, (sz, ))
 
-    model = Model()
-    expect = model(x, indices)
+    model = helpers.ModelWithWeights(op, x.shape)
+    native_out, _ = model((x, indices))
 
-    pop_model = poptorch.inferenceModel(model)
-    actual = pop_model(x, indices)
+    poptorch_model = poptorch.trainingModel(model)
+    poptorch_out, _ = poptorch_model((x, indices))
 
-    helpers.assert_allclose(actual=actual, expected=expect)
+    # Inference test - check outputs
+    helpers.assert_allclose(actual=poptorch_out, expected=native_out)
+
+    # Training test - check weights changed
+    poptorch_model.assert_weights_changed()
