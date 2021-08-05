3e99c291908bc03b70c2e2e46c6bd6cf49e5f6ac
Oskar Bunyan <oskarb@graphcore.ai>
2021-08-05 17:46:35 +0100

    Add scheduleNonWeightUpdateGradientConsumersEarly _Popart option
    Reviewers: #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, #pytorch, oskarb, hatemh
    
    Reviewed By: #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, #pytorch, hatemh
    
    Subscribers: ahmads
    
    Maniphest Tasks: T43736
    
    Differential Revision: https://phabricator.sourcevertex.net/D49270

--- popart_compiler/source/SessionOptions.cpp
+++ popart_compiler/source/SessionOptions.cpp
@@ -360,6 +360,7 @@ SessionOptionsImpl::SessionOptionsImpl() {
   ADD_POPART_BOOL_OPTION(instrumentWithHardwareCycleCounter);
   ADD_POPART_BOOL_OPTION(enableSupportedDataTypeCasting);
   ADD_POPART_BOOL_OPTION(groupNormStridedChannelGrouping);
+  ADD_POPART_BOOL_OPTION(scheduleNonWeightUpdateGradientConsumersEarly);
 
   ADD_POPART_DOUBLE_OPTION(outlineSequenceBreakCost);
   ADD_POPART_DOUBLE_OPTION(outlineThreshold);
