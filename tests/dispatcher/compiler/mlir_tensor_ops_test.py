c03633f35fdf2fb4d47af9c70ac965951e5dc455
Ahmad Sarvmeily <ahmads@graphcore.ai>
2021-11-22 11:55:43 +0000

    Add torch.stack compiler test
    Summary:
    Was already working
    
    Depends on D56090
    
    Reviewers: #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, #pytorch, nicholasw
    
    Reviewed By: #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, #pytorch, nicholasw
    
    Maniphest Tasks: T49236
    
    Differential Revision: https://phabricator.sourcevertex.net/D56102

--- tests/dispatcher/compiler/mlir_tensor_ops_test.py
+++ tests/dispatcher/compiler/mlir_tensor_ops_test.py
@@ -6,26 +6,44 @@ import helpers
 import poptorch
 from poptorch import enums
 
-cat_params = [
-    # dim, dtype, alias
-    (0, torch.float32, torch.cat),
-    (1, torch.int32, torch.concat),
-]
 
-
-@pytest.mark.skipif(not poptorch.hasMlirSupportOnPlatform(),
-                    reason="CentOS 7.3 is not currently supported in MLIR.")
-@pytest.mark.parametrize("params", cat_params)
-def test_cat(params):
-    dim, dtype, alias = params
+def cat_stack_harness(op, dim, dtype):
     t1 = torch.ones(3, 2, dtype=dtype)
     t2 = torch.zeros(3, 2, dtype=dtype)
 
-    torch_out = alias((t1, t2), dim)
+    torch_out = op((t1, t2), dim)
 
     with poptorch.IPUScope([t1, t2], compile_using=enums.Compiler.MLIR) as ipu:
-        out = alias((t1, t2), dim)
+        out = op((t1, t2), dim)
         ipu.outputs([out])
 
     # pylint: disable=no-member
     helpers.assert_allequal(actual=ipu(t1, t2), expected=torch_out)
+
+
+@pytest.mark.skipif(not poptorch.hasMlirSupportOnPlatform(),
+                    reason="CentOS 7.3 is not currently supported in MLIR.")
+@pytest.mark.parametrize(
+    "params",
+    [
+        # dim, dtype, alias
+        (0, torch.float32, torch.cat),
+        (1, torch.int32, torch.concat),
+    ])
+def test_cat(params):
+    dim, dtype, alias = params
+    cat_stack_harness(alias, dim, dtype)
+
+
+@pytest.mark.skipif(not poptorch.hasMlirSupportOnPlatform(),
+                    reason="CentOS 7.3 is not currently supported in MLIR.")
+@pytest.mark.parametrize(
+    "params",
+    [
+        # dim, dtype
+        (0, torch.float32),
+        (1, torch.int32),
+    ])
+def test_stack(params):
+    dim, dtype = params
+    cat_stack_harness(torch.stack, dim, dtype)
