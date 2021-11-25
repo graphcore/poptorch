aece7ed382029d979f358e6fb05dd09236c261ed
Nicholas Westlake <nicholasw@graphcore.ai>
2021-11-25 15:03:37 +0000

    Add extra tests for popdist
    Reviewers: #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, #pytorch, anthonyb
    
    Reviewed By: #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, #pytorch, anthonyb
    
    Maniphest Tasks: T50978
    
    Differential Revision: https://phabricator.sourcevertex.net/D56444

---
+++ tests/popdist_test.py
@@ -0,0 +1,68 @@
+# Copyright (c) 2021 Graphcore Ltd. All rights reserved.
+
+import pytest
+
+import poptorch
+
+
+# pylint: disable=import-outside-toplevel
+def test_blocked_options():
+    import popdist.poptorch
+    opts = popdist.poptorch.Options(ipus_per_replica=2)
+
+    with pytest.raises(
+            RuntimeError,
+            match=r"Cannot call `useIpuId` with popdist\.poptorch\.Options"):
+        opts.useIpuId(1)
+
+    with pytest.raises(RuntimeError,
+                       match=r"Cannot call `replicationFactor` with " +
+                       r"popdist\.poptorch\.Options"):
+        opts.replicationFactor(1)
+
+    with pytest.raises(RuntimeError,
+                       match=r"Cannot call `Distributed.disable` with " +
+                       r"popdist\.poptorch\.Options"):
+        opts.Distributed.disable()
+
+    with pytest.raises(
+            RuntimeError,
+            match=r"Cannot call `Distributed.setEnvVarNames` with " +
+            r"popdist\.poptorch\.Options"):
+        opts.Distributed.setEnvVarNames("A", "B")
+
+    with pytest.raises(
+            RuntimeError,
+            match=r"Cannot call `Distributed.configureProcessId` with " +
+            r"popdist\.poptorch\.Options"):
+        opts.Distributed.configureProcessId(1)
+
+
+# pylint: disable=import-outside-toplevel
+def test_getters():
+    import popdist.poptorch
+    opts = popdist.poptorch.Options(ipus_per_replica=2)
+
+    assert opts.Distributed.processId == 0
+    assert opts.Distributed.numProcesses == 1
+
+
+# pylint: disable=protected-access,import-outside-toplevel
+@pytest.mark.skipif(not poptorch.ipuHardwareIsAvailable(),
+                    reason="Might error on no IPUs found")
+def test_to_dict():
+    import popdist.poptorch
+    opts = popdist.poptorch.Options(ipus_per_replica=2)
+    opts.outputMode(poptorch.enums.OutputMode.All)
+    opts.toDict()
+
+    # Should not be frozen here
+    opts.checkIsFrozen()
+
+    opts._freeze()
+
+    # Should unfeeze and freeze again
+    opts.toDict()
+
+    with pytest.raises(AttributeError, match=r"Can't modify frozen Options"):
+        opts.checkIsFrozen()
