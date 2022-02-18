graphcore/poptorch

commit f8f897b992a55ac378014a8dc59be89bd6784a6e

Hotfix for serialised MatMuls
Reviewers: #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, #pytorch, hatemh

Reviewed By: #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, #pytorch, hatemh

Maniphest Tasks: T31133

Differential Revision: https://phabricator.sourcevertex.net/D37185

poptorch/source/popart_canonicalization/BlasOps.cpp
@@ -5,6 +5,8 @@
 #include "poptorch_logging/Error.hpp"
 #include "poptorch_logging/Logging.hpp"
 
+#include "../PoptorchSymbols.hpp"
+
 namespace poptorch {
 namespace {
 
@@ -76,6 +78,12 @@ torch::jit::Node *matmulHandler(torch::jit::Graph *graph,
     // Add the trace to ease debugging for before and after IRs
     logging::trace("Replacing matmul {} with {} {} {}", *node, *merge_mat, *mul,
                    *result);
+
+    for (const torch::jit::Use &use : node->output()->uses()) {
+      if (use.user->kind() == symbols::poptorch::set_matmul_serialization) {
+        use.user->replaceInput(0, mul->output());
+      }
+    }
   } else {
     // The "normal" matmul will follow the original path
     result = createMatmul(graph, {matrix_a, matrix_b});

commit be439e3242f29bc6e2e410cc6e8704e42593b4c8

Fix serializedMatMul output size
Summary:
- Fix bug introduced by D37185 which incorrectly sets the output
  to the matmul and not the post matmul reshape (which is needed
  in some cases with unity dimensions).
- Add a test for such a case.
- Lower scalars as 1D not 0D tensors so that Onnx shape inferiors
  succeeds.

Reviewers: #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, #pytorch, ahmads

Reviewed By: #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, #pytorch, ahmads

Maniphest Tasks: T55635

Differential Revision: https://phabricator.sourcevertex.net/D61071

poptorch/source/popart_canonicalization/BlasOps.cpp
@@ -84,7 +84,7 @@ torch::jit::Node *matmulHandler(torch::jit::Graph *graph,
 
     for (const torch::jit::Use &use : node->output()->uses()) {
       if (use.user->kind() == symbols::poptorch::set_matmul_serialization) {
-        use.user->replaceInput(0, mul->output());
+        use.user->replaceInput(0, result->output());
       }
     }
   } else {

tests/blas_test.py
@@ -3,12 +3,11 @@
 
 import pytest
 import torch
-import torch.optim as optim
 import poptorch
 import helpers
 
 
-def blas_op(op, input1, input2, out, trace_model):
+def blas_op(op, input1, input2, out, trace_model, atol=1e-04, rtol=1e-04):
     class Model(torch.nn.Module):
         def __init__(self, op):
             super(Model, self).__init__()
@@ -32,14 +31,14 @@ def blas_op(op, input1, input2, out, trace_model):
 
     helpers.assert_allclose(expected=native_out,
                             actual=poptorch_out,
-                            atol=1e-05,
-                            rtol=1e-05,
+                            atol=atol,
+                            rtol=rtol,
                             equal_nan=True)
     if out is not None:
         helpers.assert_allclose(expected=native_out,
                                 actual=out,
-                                atol=1e-05,
-                                rtol=1e-05,
+                                atol=atol,
+                                rtol=rtol,
                                 equal_nan=True)
 
 
@@ -53,6 +52,39 @@ def test_matmul(optional_out, trace_model):
     out = torch.randn([10, 45]) if optional_out else None
 
     blas_op(torch.matmul, input1, input2, out, trace_model)
+
+
+@pytest.mark.parametrize("trace_model", [True, False])
+@pytest.mark.parametrize("mode",
+                         (poptorch.MatMulSerializationMode.InputChannels,
+                          poptorch.MatMulSerializationMode.ReducingDim,
+                          poptorch.MatMulSerializationMode.OutputChannels,
+                          poptorch.MatMulSerializationMode.Disabled))
+@pytest.mark.parametrize("factor", (2, 5, 10))
+@pytest.mark.parametrize("keep_precision", [True, False])
+def test_serializedMatMul(trace_model, mode, factor, keep_precision):
+    torch.manual_seed(42)
+
+    input1 = torch.rand(1, 10, 200)
+    input2 = torch.rand(200, 45)
+
+    def serialise_matmal_op(input, other, out):
+        assert out is None
+        return poptorch.serializedMatMul(input, other, mode, factor,
+                                         keep_precision)
+
+    if keep_precision:
+        input1 = input1.half()
+        input2 = input2.half()
+        blas_op(serialise_matmal_op,
+                input1,
+                input2,
+                None,
+                trace_model,
+                rtol=0.01,
+                atol=0.05)
+    else:
+        blas_op(serialise_matmal_op, input1, input2, None, trace_model)
 
 
 @pytest.mark.parametrize("optional_out", [True, False])
@@ -86,7 +118,7 @@ def test_matmul_training(bias):
     model = Net()
     opts = poptorch.Options()
 
-    optimizer = optim.SGD(model.parameters(), lr=0.01)
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
     torch.manual_seed(42)
     poptorch_model = poptorch.trainingModel(model, opts, optimizer)
     x = torch.randn(N, M, K)