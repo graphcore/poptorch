bd419e3d951a8d453c4e288b336de51400a71c35
Charlie West-Taylor <charliew@graphcore.ai>
2022-04-19 16:46:19 +0100

    Pin typing_extensions to version 4.1.1
    Summary: Later versions remove Python 3.6 support.
    
    Reviewers: #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, #pytorch, anthonyb
    
    Reviewed By: #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, #pytorch, anthonyb
    
    Maniphest Tasks: T60763
    
    Differential Revision: https://phabricator.sourcevertex.net/D65338

--- config.buildenv.py
+++ config.buildenv.py
@@ -16,6 +16,7 @@ installers.add(
         "pytest=6.2.1",
         "setuptools=58.0.4",
         "spdlog=1.8.0",
+        "typing_extensions=4.1.1",
         "wheel=0.34.2",
         "zip=3.0",
     ))
