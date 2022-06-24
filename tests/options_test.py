4bd507ff2705966d6abf152e721cf9f7adb572c9
Jack Haughton <jackha@graphcore.ai>
2022-06-24 15:11:26 +0100

    Remove unnecessary test skip
    Reviewers: #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, #pytorch, anthonyb
    
    Reviewed By: #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, #pytorch, anthonyb
    
    Maniphest Tasks: T57195
    
    Differential Revision: https://phabricator.sourcevertex.net/D69960

--- tests/options_test.py
+++ tests/options_test.py
@@ -401,10 +401,6 @@ def test_tensor_location():
 @helpers.overridePoptorchLogLevel("TRACE")
 @pytest.mark.parametrize("trace_model", [True, False])
 def test_running_statistics(capfd, dtype, setting, trace_model):
-    # Just remove the if + skip once the dispatcher is enabled
-    if not trace_model:
-        pytest.skip("TODO(T57195): requires dispatcher to be enabled")
-
     x = torch.randn((16, 16), dtype=dtype)
 
     model = torch.nn.Sequential()
