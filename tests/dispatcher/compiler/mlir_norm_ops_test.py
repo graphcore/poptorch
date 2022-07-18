62559b26cbfe30df7f530ee679cc79765b212c07
Nicholas Westlake <nicholasw@graphcore.ai>
2022-07-18 11:29:49 +0100

    Add MLIR test for instance norm
    Reviewers: #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, #pytorch, ahmads
    
    Reviewed By: #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, #pytorch, ahmads
    
    Maniphest Tasks: T49239
    
    Differential Revision: https://phabricator.sourcevertex.net/D71510

--- tests/dispatcher/compiler/mlir_norm_ops_test.py
+++ tests/dispatcher/compiler/mlir_norm_ops_test.py
@@ -162,6 +162,65 @@ def test_group_norm_train():
 
 
 @pytest.mark.mlirSupportRequired
+@pytest.mark.parametrize(
+    "instance_norm, affine, track_running_stats, training", [
+        (nn.InstanceNorm1d, True, False, False),
+        (nn.InstanceNorm2d, True, False, True),
+        (nn.InstanceNorm3d, True, True, True),
+        (nn.InstanceNorm1d, False, False, False),
+        (nn.InstanceNorm2d, False, False, True),
+        (nn.InstanceNorm3d, False, True, True),
+    ])
+def test_instance_norm(instance_norm, affine, track_running_stats, training):
+    # (Instance norm is cast to batch_norm within PyTorch)
+
+    torch.manual_seed(42)
+    C = 4
+    input_shape = [3, C, 5]
+    if instance_norm in (nn.InstanceNorm2d, nn.InstanceNorm3d):
+        input_shape.append(6)
+    if instance_norm is nn.InstanceNorm3d:
+        input_shape.append(7)
+
+    t = torch.rand(input_shape, requires_grad=True)
+    # Make the mean and stdev varied throughout
+    t = t + torch.randint(0, 5, input_shape)
+
+    ipu_norm = instance_norm(C,
+                             affine=affine,
+                             track_running_stats=track_running_stats)
+
+    # Ensure the params are not default (0 and 1)
+    if affine:
+        torch.nn.init.uniform_(ipu_norm.weight, 0.1, 2.0)
+        torch.nn.init.uniform_(ipu_norm.bias, -1.0, 1.0)
+
+    ipu_norm.train(training)
+    cpu_norm = copy.deepcopy(ipu_norm)
+
+    t2 = torch.ones_like(t)
+
+    def cpu_step(norm, x1, x2):
+        x1.retain_grad()
+        out = norm(x1)
+        loss = torch.nn.functional.mse_loss(out, x2)
+        loss.backward()
+        ret = [out, x1.grad]
+        if affine:
+            ret.extend((norm.weight.grad, norm.bias.grad))
+        if track_running_stats:
+            ret.append(norm.running_mean)
+        return ret
+
+    ipu_result = IPUContext(cpu_step, model=ipu_norm)(ipu_norm, t, t2)
+    cpu_result = cpu_step(cpu_norm, t, t2)
+
+    # Test outputs and gradients
+    for cpu, ipu in zip(cpu_result, ipu_result):
+        helpers.assert_allclose(actual=ipu, expected=cpu)
+
+
+@pytest.mark.mlirSupportRequired
 @pytest.mark.parametrize("num_dims_normalize_start", [(2, 0), (2, 1), (3, 0),
                                                       (3, 1), (3, 2), (4, 0),
                                                       (4, 1), (4, 2), (4, 3)])
