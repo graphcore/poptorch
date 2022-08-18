9d914fb87c38a3190dac5827f999dc0f9c45e01c
Nicholas Westlake <nicholasw@graphcore.ai>
2022-08-18 11:07:49 +0100

    Support lstm forward with MLIR compiler
    Summary:
    - Add lowerToPoplar implementation for lstm_input
    - Fix missing dropout check
    - Fix some typos in RNN.td
    
    Reviewers: #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, #pytorch, anthonyb
    
    Reviewed By: #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, #pytorch, anthonyb
    
    Subscribers: georgew, anthonyb
    
    Maniphest Tasks: T65033
    
    Differential Revision: https://phabricator.sourcevertex.net/D73799

--- poptorch_compiler/lower_to_poplar/ops/RNNOps.cpp
+++ poptorch_compiler/lower_to_poplar/ops/RNNOps.cpp
@@ -1,5 +1,9 @@
 // Copyright (c) 2022 Graphcore Ltd. All rights reserved.
 
+#include <poplar/Graph.hpp>
+#include <popnn/Lstm.hpp>
+#include <popops/ElementWise.hpp>
+
 #include "poptorch_logging/Error.hpp"
 
 #include "../CompilerHelpers.hpp"
@@ -7,8 +11,141 @@
 namespace poptorch_ir {
 
 void lstm_input::lowerToPoplar(CompilerContext &context) {
-  context.fromSsa(this->input());
-  ERROR("Not yet implemented");
+  std::vector<poplar::Tensor> hidden_layers = context.fromSsa(this->hx());
+  std::vector<poplar::Tensor> params = context.fromSsa(this->params());
+
+  ERROR_ON(!this->has_biases());
+  ERROR_ON(this->num_layers() != 1);
+  ERROR_ON(this->dropout().convertToFloat() != 0.0);
+
+  poplar::Tensor raw_input = context.fromSsa(this->input());
+  // Poplibs expects order [sequence_length, batch_size, input_size].
+  if (this->batch_first()) {
+    raw_input = raw_input.dimShuffle({1, 0, 2});
+  }
+
+  // An LSTM state is made of 4 values
+  constexpr std::uint64_t state_size = 4;
+  ERROR_ON(params[0].shape()[0] % 4 != 0);
+  const std::uint64_t hidden_size = params[0].shape()[0] / state_size;
+
+  // Prepare the weights
+
+  // Use this function to obtain a view on the poplibs tensors suitable for
+  // copying PyTorch tensors
+  auto pytorch_view_of = [&hidden_size](const poplar::Tensor &poplibs_tensor,
+                                        bool are_weights) {
+    // Poplibs needs weights to be shape [state_size, x_size, hidden_size]
+    // and biases to be shape [state_size, hidden_size]
+    // where x_size is either input_size or hidden_size
+
+    // PyTorch weights are shape [state_size*hidden_size, x_size]
+    // Pytorch biases are shape [state_size*hidden_size]
+
+    // Perform the slice and shuffle here. Note that this is the inverse, i.e.
+    // take the poplibs weight and transform it into the PyTorch weight.
+
+    // Both input-hidden and hidden-hidden weights need to be transposed the
+    // same way, removing the ambiguity of hidden_size used for both dims.
+
+    std::vector<poplar::Interval> intervals{{0, 1}, {1, 2}, {2, 3}, {3, 4}};
+    std::vector<poplar::Tensor> slices = poplibs_tensor.slices(intervals, 0);
+
+    // Transpose h_size and hidden_size if weights (not biases)
+    if (are_weights) {
+      for (size_t i = 0; i < slices.size(); i++) {
+        slices[i] = slices[i].dimShuffle({0, 2, 1});
+      }
+    }
+
+    // Poplibs needs order FICO while PyTorch uses order IFCO
+    // i - input gate o - output gate f - forget gate c - cell gate
+    poplar::Tensor wf = slices[0];
+    poplar::Tensor wi = slices[1];
+    poplar::Tensor wc = slices[2];
+    poplar::Tensor wo = slices[3];
+
+    poplar::Tensor concat = poplar::concat({wi, wf, wc, wo}, 0);
+
+    poplar::Tensor reshaped;
+    if (are_weights) {
+      reshaped =
+          concat.reshape({state_size * hidden_size,
+                          concat.numElements() / (state_size * hidden_size)});
+    } else {
+      reshaped = concat.reshape({state_size * hidden_size});
+    }
+
+    return reshaped;
+  };
+
+  const popnn::lstm::LstmParams lstm_params(
+      raw_input.elementType(), raw_input.shape()[1], raw_input.shape()[0],
+      {raw_input.shape()[2], hidden_size});
+
+  // Create poplib weights and copy the pytorch weights
+  popnn::lstm::LstmWeights weights = popnn::lstm::createWeights(
+      context.graph, lstm_params, poplar::DebugContext("LstmWeights"));
+  context.seq.add(poplar::program::Copy(
+      params[0], pytorch_view_of(weights.inputWeights, true)));
+  context.seq.add(poplar::program::Copy(
+      params[1], pytorch_view_of(weights.outputWeights, true)));
+
+  // Poplibs LSTM takes a sum of the respective biases
+  poplar::Tensor pytorch_biases =
+      popops::add(context.graph, params[2], params[3], context.seq);
+  context.seq.add(poplar::program::Copy(
+      pytorch_biases, pytorch_view_of(weights.biases, false)));
+
+  // Handle initial state
+
+  // Ignore matmul planning cache and options for now
+  popnn::lstm::LstmState lstm_state = popnn::lstm::createInitialState(
+      context.graph, lstm_params, poplar::DebugContext("LstmInitialState"));
+
+  ERROR_ON(hidden_layers.size() != 2);
+  poplar::Tensor init_h = lstm_state.output;
+  context.seq.add(poplar::program::Copy(hidden_layers[0], init_h, false));
+
+  poplar::Tensor init_c = lstm_state.cellState;
+  context.seq.add(poplar::program::Copy(hidden_layers[1], init_c, false));
+
+  // Always copy input for now
+  poplar::Tensor input = context.graph.clone(raw_input);
+  context.seq.add(poplar::program::Copy(raw_input, input));
+
+  poplar::Tensor output;
+  poplar::Tensor cell_state;
+
+  // TODO(T68091) support set_available_memory
+
+  std::tie(output, cell_state) = popnn::lstm::lstmFwd(
+      context.graph, lstm_params, lstm_state, input, weights, nullptr,
+      context.seq, poplar::DebugContext("LstmFwd"));
+
+  // The Poplibs output will be [sequence_length, batch_size, input_size]
+  // This is correct for Pytorch except if batch_first is specified.
+  if (this->batch_first()) {
+    context.addTensor(this->output(), output.dimShuffle({1, 0, 2}));
+  } else {
+    context.addTensor(this->output(), output);
+  }
+
+  // h_n needs to be size [1, batch_size, hidden_size]
+  // (1 as we support only 1 layer and no bidirectional)
+  // For one layer, this is simply the output at the last time step.
+  poplar::Tensor output_h_state =
+      output.slice(lstm_params.timeSteps - 1, lstm_params.timeSteps, 0);
+  context.addTensor(
+      this->hy(),
+      output_h_state.reshape(
+          {1, static_cast<std::uint64_t>(lstm_params.batchSize), hidden_size}));
+
+  // c_n also needs the same resize
+  context.addTensor(
+      this->cy(),
+      cell_state.reshape(
+          {1, static_cast<std::uint64_t>(lstm_params.batchSize), hidden_size}));
 }
 
 } // namespace poptorch_ir
---
+++ tests/dispatcher/compiler/mlir_rnn_ops_test.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python3
+# Copyright (c) 2021 Graphcore Ltd. All rights reserved.
+import copy
+import torch
+from torch import nn
+import pytest
+import helpers
+from poptorch.experimental import IPUContext
+
+
+@pytest.mark.mlirSupportRequired
+@pytest.mark.parametrize("lstm_config", [
+    {
+        'batch_size': 2,
+        'batch_first': False,
+        'sequence_length': 5,
+        'input_size': 3,
+        'hidden_size': 4
+    },
+    {
+        'batch_size': 6,
+        'batch_first': False,
+        'sequence_length': 5,
+        'input_size': 3,
+        'hidden_size': 4
+    },
+    {
+        'batch_size': 2,
+        'batch_first': True,
+        'sequence_length': 5,
+        'input_size': 3,
+        'hidden_size': 4
+    },
+    {
+        'batch_size': 2,
+        'batch_first': False,
+        'sequence_length': 6,
+        'input_size': 3,
+        'hidden_size': 4
+    },
+    {
+        'batch_size': 2,
+        'batch_first': False,
+        'sequence_length': 5,
+        'input_size': 6,
+        'hidden_size': 4
+    },
+    {
+        'batch_size': 2,
+        'batch_first': False,
+        'sequence_length': 5,
+        'input_size': 3,
+        'hidden_size': 6
+    },
+    {
+        'batch_size': 1,
+        'batch_first': False,
+        'sequence_length': 5,
+        'input_size': 3,
+        'hidden_size': 4
+    },
+])
+def test_lstm(lstm_config):
+    num_layers = 1
+
+    torch.manual_seed(42)
+
+    if lstm_config['batch_size'] == "1":
+        input_shape = [
+            lstm_config['sequence_length'], lstm_config['input_size']
+        ]
+        output_shape = [
+            lstm_config['sequence_length'], lstm_config['hidden_size']
+        ]
+        pytest.skip()  # TODO(T57255) unbatched input supported in torch 1.12
+
+    if lstm_config['batch_first']:
+        input_shape = [
+            lstm_config['batch_size'], lstm_config['sequence_length'],
+            lstm_config['input_size']
+        ]
+        output_shape = [
+            lstm_config['batch_size'], lstm_config['sequence_length'],
+            lstm_config['hidden_size']
+        ]
+    else:
+        input_shape = [
+            lstm_config['sequence_length'], lstm_config['batch_size'],
+            lstm_config['input_size']
+        ]
+        output_shape = [
+            lstm_config['sequence_length'], lstm_config['batch_size'],
+            lstm_config['hidden_size']
+        ]
+
+    input_t = torch.randn(input_shape)
+    target_t = torch.randn(output_shape)
+
+    ipu_lstm = nn.LSTM(input_size=lstm_config['input_size'],
+                       hidden_size=lstm_config['hidden_size'],
+                       num_layers=num_layers,
+                       batch_first=lstm_config['batch_first'])
+
+    cpu_lstm = copy.deepcopy(ipu_lstm)
+
+    def cpu_step(lstm, x1, x2):
+        x1.requires_grad = True
+        x1.retain_grad()
+        out, (h_n, c_n) = lstm(x1)
+        loss = torch.nn.functional.mse_loss(out, x2)
+        loss.backward()
+        ret = [out, h_n, c_n]
+
+        # For now, grad is None, in future return these
+        #  x1.grad, lstm.weight_ih_l0.grad,
+        #        lstm.weight_hh_l0.grad, lstm.bias_ih_l0.grad,
+        #        lstm.bias_hh_l0.grad
+        return ret
+
+    ipu_result = IPUContext(cpu_step, model=ipu_lstm)(ipu_lstm, input_t,
+                                                      target_t)
+
+    cpu_result = cpu_step(cpu_lstm, input_t, target_t)
+
+    # Test outputs and gradients
+    for cpu, ipu in zip(cpu_result, ipu_result):
+        helpers.assert_allclose(actual=ipu,
+                                expected=cpu)  #, rtol=1e-3, atol=1e-3)
