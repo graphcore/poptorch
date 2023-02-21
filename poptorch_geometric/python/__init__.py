abd62031385b8324ee11015de97116fca527d5bd
Bartlomiej Wroblewski <bartlomiejw@graphcore.ai>
2023-02-21 08:28:08 +0000

    Remove the implementation of SchNet model
    Summary: !ci_branch_mk2
    
    Reviewers: #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, #pytorch, jacekr
    
    Reviewed By: #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, #pytorch, jacekr
    
    JIRA Issues: AFS-163
    
    Differential Revision: https://phabricator.sourcevertex.net/D80974

--- poptorch_geometric/python/__init__.py
+++ poptorch_geometric/python/__init__.py
@@ -2,14 +2,13 @@
 from .collate import make_exclude_keys
 from .dataloader import create_fixed_batch_dataloader
 from .pad import Pad
-from .schnet import SchNet
 from .types import PyGArgsParser
 from .utils import TrainingStepper, set_aggregation_dim_size
 
 __version__ = "@VERSION@-@SNAPSHOT@"
 
 __all__ = [
-    '__version__', 'create_fixed_batch_dataloader', 'Pad', 'SchNet',
+    '__version__', 'create_fixed_batch_dataloader', 'Pad',
     'set_aggregation_dim_size', 'TrainingStepper', 'make_exclude_keys',
     'PyGArgsParser'
 ]
