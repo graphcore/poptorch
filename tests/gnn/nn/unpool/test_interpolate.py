fdebff8452b1f9930fa7a7ac5053976ff86707d2
Piotr Chmiel <piotrc@graphcore.ai>
2023-04-03 14:26:36 +0100

    Temporary skip failing test
    Summary: !ci_branch_mk2
    
    Reviewers: #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, #pytorch, kamil.andrzejewski
    
    Reviewed By: #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, #pytorch, kamil.andrzejewski
    
    JIRA Issues: AFS-195
    
    Differential Revision: https://phabricator.sourcevertex.net/D82521

--- tests/gnn/nn/unpool/test_interpolate.py
+++ tests/gnn/nn/unpool/test_interpolate.py
@@ -1,5 +1,5 @@
 # Copyright (c) 2023 Graphcore Ltd. All rights reserved.
-
+import pytest
 import helpers
 
 import torch
@@ -9,6 +9,7 @@ import poptorch
 import poptorch_geometric  # pylint: disable=unused-import
 
 
+@pytest.mark.skip()
 def test_knn_interpolate():
     x = torch.Tensor([[1], [10], [100], [-1], [-10], [-100]])
     pos_x = torch.Tensor([[-1, 0], [0, 0], [1, 0], [-2, 0], [0, 0], [2, 0]])
