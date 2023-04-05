7ee905332fc7a7b6fd1c4e7cd8328ebfe4b883a6
Bartlomiej Wroblewski <bartlomiejw@graphcore.ai>
2023-04-05 13:44:54 +0100

    Enable GEN conv by replacing bnorm with lnorm
    Reviewers: #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, #pytorch, kamil.andrzejewski
    
    Reviewed By: #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, #pytorch, kamil.andrzejewski
    
    JIRA Issues: AFS-187
    
    Differential Revision: https://phabricator.sourcevertex.net/D82602

--- tests/gnn/nn/conv/test_gen_conv.py
+++ tests/gnn/nn/conv/test_gen_conv.py
@@ -8,15 +8,23 @@ from conv_utils import conv_harness
 
 @pytest.mark.parametrize('aggr', ['softmax', 'powermean'])
 def test_gen_conv(aggr, dataset):
-    pytest.skip('AFS-187 Precision errors requires investigations')
     in_channels = dataset.num_node_features
 
-    conv = GENConv(in_channels, 32, aggr, edge_dim=16, add_self_loops=False)
+    conv = GENConv(in_channels,
+                   32,
+                   aggr,
+                   edge_dim=16,
+                   add_self_loops=False,
+                   norm='layer')
     conv_harness(conv, dataset)
 
     x2 = torch.randn(dataset.x.shape)
     batch = ((dataset.x, x2), dataset.edge_index)
     conv_harness(conv, dataset, batch=batch)
 
-    conv = GENConv((in_channels, in_channels), 32, aggr, add_self_loops=False)
+    conv = GENConv((in_channels, in_channels),
+                   32,
+                   aggr,
+                   add_self_loops=False,
+                   norm='layer')
     conv_harness(conv, dataset, batch=batch)
